bbe4cc9e431e03ba2ca3462740db88141619032a
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2025-12-23 20:22:35 +0100

    moduli 1

---
+++ moduli 1/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moduli 1</title>
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <h1>my first php page</h1>
+    <?php
+    echo "PHP/MYSQL";
+    ?>
+</body>
+</html>
