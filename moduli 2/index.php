683203460ff4e525f21e08c26770bb5510ddf19b
Reis Surdulli <104998870+reissurdulli@users.noreply.github.com>
2026-01-13 20:48:35 +0100

    moduli 2

---
+++ moduli 2/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$_variable = "variabla <hr>" ;
+
+echo $_variable;
+
+$_x = 1;
+$_y=23;
+
+echo $_x + $_y."<hr>";
+echo $_x - $_y."<hr>";
+echo $_x * $_y."<hr>";
+echo $_x / $_y."<hr>";
+echo $_x % $_y."<hr>";
+?>
